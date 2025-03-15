9f5d1594913d8e8fbf13705015d974005bc10fa4
vechkolkk <khournvechkol@yahoo.com>
2025-03-15 19:07:32 -0400

    Project

---
+++ app/_layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Stack } from 'expo-router'
+
+const Layout = () => {
+    return (
+      <Stack>
+        <Stack.Screen
+            name= "index"
+            options= {{
+                headerShown: false
+            }}
+        />
+        
+      </Stack>
+    )
+}
+
+
+export default Layout
